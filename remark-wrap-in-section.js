43869c230752adfefd56f05ab805ed9d132e7b2c
Jan Florian Dietrich <luhmann@users.noreply.github.com>
2017-11-21 15:22:15 +0100

    feat: Wrap subheadings in correct sections

---
+++ remark-wrap-in-section.js
@@ -0,0 +1,33 @@
+const visit = require('unist-util-visit')
+
+function wrapInSection() {
+  return transformer
+}
+
+function transformer(tree) {
+  const headingsMap = []
+  const newTree = []
+
+  visit(tree, 'heading', (node, index) => {
+    if (node.depth === 2) headingsMap.push(index)
+  })
+
+  for (let index = 0; index <= headingsMap.length; index++) {
+    const sectionStartIndex = index === 0 ? 0 : headingsMap[index - 1]
+    const sectionEndIndex =
+      index === headingsMap.length ? tree.children.length : headingsMap[index]
+    const children = tree.children.slice(sectionStartIndex, sectionEndIndex)
+
+    const wrapperNode = {
+      type: 'paragraph',
+      children,
+      data: { hName: 'section' },
+    }
+
+    newTree.push(wrapperNode)
+  }
+
+  tree.children = newTree
+}
+
+module.exports = wrapInSection
