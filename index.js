2b492d057c025d652d635a57a13d5a0e4fffed0c
Jan Florian Dietrich <luhmann@users.noreply.github.com>
2017-11-21 13:04:43 +0100

    feat(typography): Add typographic fixes

--- index.js
+++ index.js
@@ -3,22 +3,32 @@ const path = require('path')
 const unified = require('unified')
 const markdown = require('remark-parse')
 const html = require('remark-html')
-var highlight = require('remark-highlight.js')
+const highlight = require('remark-highlight.js')
 const math = require('remark-math')
 const katex = require('remark-html-katex')
 const report = require('vfile-reporter')
+const slug = require('remark-slug')
+const frontmatter = require('remark-frontmatter')
+const textr = require('remark-textr')
 const sidenotes = require('./remark-sidenotes')
 const toSection = require('./remark-p-to-section')
 
+const textrBase = require('typographic-base')
+
 const md = fs.readFileSync(path.resolve(__dirname, 'tufte.md'))
 
-const tree = unified()
+unified()
   .use(markdown)
   .use(math)
   .use(katex)
   .use(highlight)
+  .use(slug)
   .use(sidenotes)
-  .use(toSection)
+  .use(frontmatter)
+  .use(textr, {
+    plugins: [textrBase],
+  })
+  // .use(toSection)
   .use(html)
   //   .parse(md);
   .process(md, (err, file) => {
@@ -26,7 +36,3 @@ const tree = unified()
     fs.writeFileSync(path.resolve(__dirname, 'output.html'), String(file))
     console.log(String(file))
   })
-
-// console.dir(tree, { depth: null });
-
-// tree.run();
